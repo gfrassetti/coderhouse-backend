508a3597b5ae21461fda7cd1f53876b1cc941ece
Guido Frassetti <78879523+gfrassetti@users.noreply.github.com>
2022-12-15 20:24:30 -0300

    Validaciones en addProduct

--- 3.Nuevas funcionalidades de los lenguajes ECMAScript/3.Nuevas funcionalidades de los lenguajes ECMAScript.js
+++ 3.Nuevas funcionalidades de los lenguajes ECMAScript/3.Nuevas funcionalidades de los lenguajes ECMAScript.js
@@ -27,7 +27,7 @@ class ProductManager {
 
     this.isFound = this.products.some(element => element.id === this.product.id)
     
-    if(this.isFound)
+    if(this.isFound || title != undefined || description != undefined || price != undefined || thumbnail != undefined || stock != undefined)
     {
       console.log("Element with id already in list")
       this.getProducts()
